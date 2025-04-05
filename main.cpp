c7e754761d968f4377140d04b5a1904ecf277689
Someguy7783 <adhitya6860@gmail.com>
2025-04-04 19:08:12 -0700

    made basic fraction function

--- main.cpp
+++ main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
 #include <math.h>
 #include <valarray>
-
+// Adhitya Padmanabhan
+/*
+Class notes:
+-	Important for projects:
+o	Comments to understand
+o	Organization/structure
+o	Code works
+o	Readable/syntax
+o	Efficient
+-	Comments only when code is complicated/needed
+ */
 bool is_power_of_2_loop(int);
 bool is_power_of_2_(int);
 bool is_prime(int);
@@ -24,32 +34,6 @@ public:
 };
 
 int main() {
-  int num = 5;
-  int num2 = 8;
-  int num3 = 9;
-  bool result = is_power_of_2_loop(num);
-  bool result2 = is_power_of_2_(num2);
-  bool result3 = is_prime(num3);
-  if (result == true) {
-    printf("%d is div by 2\n", 5);
-  } else {
-    printf("%d is not div by 2\n", 5);
-  }
-  // no loop
-  if (result2 == true) {
-    printf("%d is div by 2 (non loop)\n", 9);
-  } else {
-    printf("%d is not div by 2 (non loop)\n", 9);
-  }
-  if (result3 == true) {
-    //printf("%d is prime\n", 7);
-    std::cout << "PASS\n";
-  } else {
-    //printf("%d is not prime\n", 7);
-    std::cout << "FAIL\n";
-  }
-  Fraction num1(2, 4);
-  std::cout << num1.to_string() << "\n";
   test();
   return 0;
 }
@@ -88,6 +72,7 @@ bool is_prime(int num) {
 }
 
 Fraction reduce_fraction(Fraction input) {
+  //improve
   int div = 2;
   while (div <= input.num && div <= input.den) {
     if (input.num % div == 0 && input.den % div == 0) {
@@ -101,6 +86,32 @@ Fraction reduce_fraction(Fraction input) {
 }
 
 void test() {
+  int num = 8;
+  int num2 = 9;
+  int num3 = 9;
+  bool result = is_power_of_2_loop(num);
+  bool result2 = is_power_of_2_(num2);
+  bool result3 = is_prime(num3);
+  if (result == true) {
+    printf("%d is div by 2\n", 8);
+  } else {
+    printf("%d is not div by 2\n", 8);
+  }
+  // no loop
+  if (result2 == true) {
+    printf("%d is div by 2 (non loop)\n", 9);
+  } else {
+    printf("%d is not div by 2 (non loop)\n", 9);
+  }
+  if (result3 == true) {
+    //printf("%d is prime\n", 7);
+    std::cout << "PASS\n";
+  } else {
+    //printf("%d is not prime\n", 7);
+    std::cout << "FAIL\n";
+  }
+  Fraction num1(2, 4);
+  std::cout << num1.to_string() << "\n";
   //reduce to min terms
   //given input, output should match expected output
   Fraction inputs[4] = {Fraction(1, 2),
